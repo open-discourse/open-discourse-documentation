246e6a8709c1452da359e29daee625bff368eaff
Florian Richter <florian.richter@limebit.de>
2021-04-12 23:29:31 +0200

    index page update

--- src/01-get_version_history.R
+++ src/01-get_version_history.R
@@ -1,6 +1,9 @@
 library(httr)
+library(dataverse)
+library(dplyr)
 
 
+# github ------------------------------------------------------------------
 github_api <- function(path) {
   url <- modify_url("https://api.github.com", path = path)
   resp <- GET(url)
@@ -9,14 +12,47 @@ github_api <- function(path) {
 
 resp <- github_api("/repos/open-discourse/open-discourse/releases")
 
-current_version_tag <- resp[[1]]$tag_name
-current_version_date <- stringr::str_extract(resp[[1]][["published_at"]], pattern = "^[^T]*")
 
+gh_version_history <- tibble::tibble()
+for(i in 1:length(resp)){
+  v <- try(resp[[i]]$tag_name)
+  t <- try(resp[[i]]$published_at)
+  gh_version_history <- rbind(gh_version_history, tibble::tibble(v = v, t = t))
+}
+
+gh_version_history <- gh_version_history %>% 
+  mutate(t = stringr::str_extract(t, pattern = "^[^T]*"),
+         t = as.Date(t)) %>% 
+  arrange(desc(t))
+
+current_version_tag <- gh_version_history[[1]]
+current_version_date <- gh_version_history[[2]]
 
 #create target folder if not available
-
 ifelse(!dir.exists(paste0("docs/", current_version_tag)), 
        dir.create(paste0("docs/", current_version_tag)), FALSE)
+
+
+# hdv ---------------------------------------------------------------------
+server <- "dataverse.harvard.edu"
+doi <- "doi:10.7910/DVN/FIKIBO"
+
+od_versions <- dataset_versions(dataset = doi, 
+                                server = server, 
+                                key = key)
+
+hdv_version_history <- tibble::tibble()
+for(i in 1:length(od_versions)){
+  v <- try(od_versions[[i]]$versionNumber)
+  t <- try(od_versions[[i]]$releaseTime)
+  hdv_version_history <- rbind(hdv_version_history, tibble::tibble(v = v, t = t))
+}
+
+hdv_version_history <- hdv_version_history %>% 
+  mutate(t = stringr::str_extract(t, pattern = "^[^T]*"),
+         t = as.Date(t)) %>% 
+  arrange(desc(t))
+
 
 
 
